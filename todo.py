2d790644743cd1e02ea3e4af9c112adfa76a9aca
uzairidris <uzairidris4812@gmail.com>
2023-03-17 20:52:23 -0400

    added search bar feature

--- todo.py
+++ todo.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter.font import Font
 import datetime
 
+
 # Initialize the root and title as well as geometry
 root = Tk()
 root.title("To-Do List")
@@ -10,6 +11,32 @@ root.geometry("500x500")
 
 # Create a font for the items in the list
 font = Font(family="Comic Sans MS", size=20, weight="bold")
+
+# Create a frame for the search box and search button
+search_frame = Frame(root)
+search_frame.pack(pady=5)
+
+# Create an entry box for the search
+search_box = tk.Entry(search_frame, width=35)
+search_box.insert(0, "Search for a task using keywords")
+
+
+# Create commands for the search box
+def on_entry_click(event):
+    if search_box.get() == "Search for a task using keywords":
+        search_box.delete(0, tk.END)
+        search_box.insert(0, '')
+
+
+def on_focusout(event):
+    if search_box.get() == '':
+        search_box.insert(0, "Search for a task using keywords")
+
+
+search_box.bind('<FocusIn>', on_entry_click)
+search_box.bind('<FocusOut>', on_focusout)
+search_box.pack(side=LEFT)
+
 
 # Create a frame and a Listbox for the actual to do list and pack them accordingly
 frame = tk.Frame(root)
@@ -69,6 +96,7 @@ def add_item():
         entry = str(num) + "- " + item + num_spaces + date_string
     if item:
         to_do_list.insert(tk.END, entry)
+        original_items.append(entry)
     num += 1
 
 
@@ -76,7 +104,9 @@ def remove_item():
     selection = to_do_list.curselection()
     if selection:
         index = selection[0]
+        item = to_do_list.get(index)
         to_do_list.delete(index)
+        original_items.remove(item)
 
     new_num = 1
     for i in range(to_do_list.size()):
@@ -99,6 +129,36 @@ check_button = tk.Button(buttonFrame, text="Check Item Off")
 add_button.grid(row=0, column=0)
 delete_button.grid(row=0, column=1)
 check_button.grid(row=0, column=2)
+
+# Keep a copy of all items in the to do list
+original_items = []
+for i in range(to_do_list.size()):
+    original_items.append(to_do_list.get(i))
+
+
+# Create a function for the search button
+def search_item():
+    search_text = search_box.get()
+    if search_text:
+        to_do_list.delete(0, tk.END)
+        for item in original_items:
+            if search_text.lower() in item.lower():
+                to_do_list.insert(tk.END, item)
+
+
+def restore_item():
+    to_do_list.delete(0, tk.END)
+    for item in original_items:
+        to_do_list.insert(tk.END, item)
+
+
+# Create a restore button for the list
+restore_button = tk.Button(search_frame, text="Restore", command=restore_item)
+restore_button.pack(side=RIGHT)
+
+# Creating the search button for the search bar
+search_button = tk.Button(search_frame, text="Search", command=search_item)
+search_button.pack(side=LEFT)
 
 
 # main loop of the program
